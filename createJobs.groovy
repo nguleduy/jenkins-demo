7067bfdc99fec20f793f575e674fdd133fd3fbae
Joseph <nguld@itsj-group.com>
2021-02-26 15:13:18 +0700

    add theme-park-job

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/nguleduy/springboot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
